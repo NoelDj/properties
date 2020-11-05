c8d1c672a96f3f237cbf401b8a3dca9f15866c06
NoelDj <noel0045@stud.kea.dk>
2020-11-06 00:46:17 +0100

    subpage finished

--- sub.js
+++ sub.js
@@ -1,46 +1,50 @@
 window.addEventListener("DOMContentLoaded", getData)
 
-const datalink = "https://quater.org/assignment/wp-json/wp/v2/property?_embed";
+const datalink2 = "https://quater.org/assignment/wp-json/wp/v2/property?_embed";
 
-function getData(){
+function getData() {
     const urlParams = new URLSearchParams(window.location.search);
     const house_id = urlParams.get("house_id");
-
+    console.log(window.location)
     console.log(house_id)
 
-
-    if(house_id) {
-        fetch("https://quater.org/assignment/wp-json/wp/v2/property/" + house_id + "?_embed");
-         fetch(datalink)
-    .then(res => res.json())
-    .then(handleData)
-
+    if (house_id) {
+        fetch("https://quater.org/assignment/wp-json/wp/v2/property/" + house_id + "?_embed")
+            .then(res => res.json())
+            .then(showPost)
+    } else if (!house_id && window.location.pathname == "/singlepage.html") {
+        window.location.replace("index.html");
+    } else {
+        fetch(datalink)
+            .then(res => res.json())
+            .then(handleData)
     }
 }
 
-function handleData(posts){
+function handleData(posts) {
     console.log(posts);
 }
 
+function showPost(post) {
+    console.log(post)
 
-/*
-const queryTwo = urlParams.get("category");
+    const template = document.querySelector("template").content;
+    const copy = template.cloneNode(true);
 
-console.log(query)
-console.log(queryTwo)*/
+    copy.querySelector("h2").textContent = post.title.rendered;
+    copy.querySelector(".bed").textContent = post.bed;
+    copy.querySelector(".bath").textContent = post.bath;
+    copy.querySelector(".sqm").textContent=post.sq_meters;
+    copy.querySelector(".sqm").textContent = post.sq_meters;
+    copy.querySelector(".houseimg").src=post._embedded['wp:featuredmedia'][0].source_url;
+
+    /*const a = copy.querySelector("a");
+    a.href += post.id;*/
+
+
+    document.querySelector("main").appendChild(copy);
 
 
 
-/*const query = urlParams.get("title");
 
-
-const queryTwo = urlParams.get("category");
-
-console.log(query)
-console.log(queryTwo)
-
-const h1 = document.querySelector("h1");
-h1.textContent += query;
-
-const h2 = document.querySelector("h2");
-h2.textContent += queryTwo;*/
+}
